4f98ffcbb15d901b0f8b1646f44426dc59c374bd
Ayman Chafni <aymanchafni@gmail.com>
2024-04-20 06:09:37 -0400

    changed chart type to line for fx points and inhanced chart quality

--- Client/metah-viz/src/App.js
+++ Client/metah-viz/src/App.js
@@ -2,50 +2,13 @@ import React, {useState, useEfect,useRef} from 'react'
 import api from './api'
 import axios from 'axios';
 import { Line } from 'react-chartjs-2';
-import Chart, { plugins } from 'chart.js/auto';
+import Chart, { elements, plugins } from 'chart.js/auto';
 import zoomPlugin from 'chartjs-plugin-zoom';
 
 const App = () => {
 
     var fx = (x) => { return 1.7781*0.625*x[1]*x[2]**2 + 0.6224*0.625*x[0]*x[2]*x[3] + 3.1661*(0.625*x[0])**2*x[3] + 19.84*(0.625*x[0])**2*x[2]
     };
-
-
-   
-    Chart.register(zoomPlugin);
-
-    const plugin1 = {
-       // id: "cc",
-        beforeInit: function(chart) {
-  
-            // We get the chart data
-            var data = chart.config.data;
-            
-            //create function vars
-            let x1 = Array.from({length: 1000}, () => Math.random() * 300);
-            let x2 = Array.from({length: 1000}, () => Math.random() * 300);
-            let x3 = Array.from({length: 1000}, () => Math.random() * 300);
-            let x4 = Array.from({length: 1000}, () => Math.random() * 300);
-
-
-                
-                // For every label ...
-            for (var j = 0; j < x1.length; j++) {
-                const x = x4[j];
-                // We get the dataset's function and calculate the value
-                var fct = data.datasets[0].function,
-                    //affect x1 to data.labels
-                    
-                    y = fct([x1[j],x2[j],x3[j],x]);
-                // Then we add the value to the dataset data
-                data.labels.push(x)
-                data.datasets[0].data.push(y);
-                
-            }
-        
-            
-        }
-    }; 
 
     const RGB_Linear_Shade=(p,c)=>{
         var i=parseInt,r=Math.round,[a,b,c,d]=c.split(","),P=p<0,t=P?0:255*p,P=P?1+p:1-p;
@@ -74,21 +37,125 @@ const App = () => {
 
     };
 
+    const quickSort = (arr) => {
+        if (arr.length <= 1) {
+          return arr;
+        }
+      
+        let pivot = arr[0];
+        let leftArr = [];
+        let rightArr = [];
+      
+        for (let i = 1; i < arr.length; i++) {
+          if (arr[i] < pivot) {
+            leftArr.push(arr[i]);
+          } else {
+            rightArr.push(arr[i]);
+          }
+        }
+      
+        return [...quickSort(leftArr), pivot, ...quickSort(rightArr)];
+      };
+      
+   
+    Chart.register(zoomPlugin);
+
+    const plugin1 = {
+       // id: "cc",
+        beforeInit: function(chart) {
+  
+            // We get the chart data
+            var data = chart.config.data;
+            
+            //create function vars
+            let x1 = Array.from({length: 1000}, () => Math.random() * 300);
+            let x2 = Array.from({length: 1000}, () => Math.random() * 300);
+            let x3 = Array.from({length: 1000}, () => Math.random() * 300);
+            let x4 = Array.from({length: 1000}, () => Math.random() * 300);
+
+            //x1 = quickSort(x1);  
+            //x2 = quickSort(x2);
+            //x3 = quickSort(x3);
+            x4 = quickSort(x4);
+
+                // For every label ...
+            for (var j = 0; j < x1.length; j++) {
+                const x = x4[j];
+                // We get the dataset's function and calculate the value
+                var fct = data.datasets[0].function,
+                    //affect x1 to data.labels
+                    
+                    y = fct([x1[j],x2[j],x3[j],x]);
+                // Then we add the value to the dataset data
+                data.labels.push(x);
+                data.datasets[0].data.push(y);
+                
+            }
+        
+            
+        }
+    }; 
+
+    const update_chart_data = (chart, data) =>{
+
+        chart.data.labels.shift();
+        chart.data.datasets[1].data.shift();
+        chart.data.datasets[0].data.shift();
+
+       
+
+                
+        chart.data.labels.splice(99,0,data.x[3]);
+        chart.data.datasets[1].data.splice(99,0,data.fx);
+        chart.data.datasets[0].data.splice(99,0,null);
+
+        var labels = chart.data.labels.slice(100);
+
+        var index = labels.length;
+        for(let i=0; i<labels.length; i++){
+            if(labels[i] > data.x[3]){
+                index = i;
+                break;
+            };
+        };
+       
+        chart.data.labels.splice(100+index,0,data.x[3]);
+        chart.data.datasets[0].data.splice(100+index,0,data.fx);
+        chart.data.datasets[1].data.push(null);
+
+       
+
+
+        chart.update();
+    };
+
+    
+
     const pointBackgroundColors =generate_point_colors(RGB_Log_Shade,100,"rgb(0, 255, 0)");
+
+    const decimation = {
+        enabled: true,
+        algorithm: 'lttb',
+        samples:50
+      };
+
     const config = {
         
         plugins: [plugin1],
+        
         data: {
             labels: Array(100).fill(null),
             datasets: [
                        {
-                       type: 'scatter',
+                       type: 'line',
                           label: "f(x)",
                           function: function(x) { return fx(x) },
                           data: Array(100).fill(null),
                           backgroundColor: 'rgb(1, 1, 1)',
                           borderColor: "rgba(1, 1, 1)",
                           fill: false,
+                          cubicInterpolationMode: 'monotone',
+                          tension: 0.4,
                           order:2
                         }, 
                        { 
@@ -99,6 +166,11 @@ const App = () => {
                             pointBackgroundColor: pointBackgroundColors,
                             data: Array(1100).fill(null),
                             fill: false,
+                            elements:{
+                                point:{
+                                    radius : 3,
+                                }
+                            },
                             order:1
                             
                         } 
@@ -108,6 +180,9 @@ const App = () => {
         ],
         },
         options: {
+             // Turn off animations and data parsing for performance
+            animation: false,
+            //parsing: false,
             responsive: true,
             title: {
                 display: true,
@@ -121,25 +196,42 @@ const App = () => {
                 mode: 'nearest',
                 intersect: true
             },
+            interaction: {
+                mode: 'nearest',
+                axis: 'x',
+                intersect: false
+              },
             scales: {
-                xAxes: [{
-                    display: false,
-                    scaleLabel: {
-                        display: false,
-                        labelString: 'X1'
+                x: {
+                    type: 'linear',
+                    title: {
+                        display: true,
+                        text: 'X1'
+                    },
+                    
+                },
+                y: {
+                    type: 'logarithmic',
+
+                    title: {
+                        display: true,
+                        text: 'Score'
                     }
-                }],
-                yAxes: [{
-                    display: false,
-                    max: 2000000,
-                    scaleLabel: {
-                        display: false,
-                        labelString: 'Score'
-                    }
-                }],
+                },
                 
             },
+            elements:{
+                point:{
+                    radius : 1,
+                },
+                line:{
+                    borderWidth:0.5,
+                    borderJoinStyle:'round'
+                }
+            },
+           
             plugins: {
+                decimation: decimation,
                 zoom: {
                   zoom: {
                     wheel: {
@@ -173,20 +265,10 @@ const App = () => {
     source.onmessage = function (event) {
         const data = JSON.parse(event.data);
       
-          if (config.data.labels.length === 1100) {
-            config.data.labels.shift();
-            config.data.datasets[1].data.shift();
-            config.data.datasets[0].data.shift();
+        update_chart_data(lineChart,data);
 
-        } 
+
         
-        config.data.labels.splice(99,0,data.x[3]);
-        config.data.datasets[1].data.splice(99,0,data.fx);
-        config.data.datasets[0].data.splice(99,0,null);
-
-    
-
-        lineChart.update();
     }; 
 
 
